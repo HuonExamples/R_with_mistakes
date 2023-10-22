33cd913a7bb399d00b228401a3690c0aee309894
Huon Research <rohan@huon.ai>
2023-10-22 18:31:14 +0000

    Updated file

--- example_1.R
+++ example_1.R
@@ -1,12 +1,14 @@
-# In this script, we didn't specify a column name for the summarize function.
+# In this script, there are a few improvements that can be made:
 
-library(dplyr)
+# 1. Add a column name for the summarize function.
+# 2. Remove the unnecessary library(dplyr) statement, as it is already loaded in the environment.
+# 3. Specify the package name (`dplyr::`) when using the `summarize()` function.
 
 data <- data.frame(
   Name = c("Alice", "Bob", "Charlie"),
   Score = c(85, 92, 78)
 )
 
-# Attempting to calculate the mean score
+# Attempting to calculate the mean score and renaming the column to "average_score"
 average_score <- data %>%
-  summarize(mean(Score))
+  dplyr::summarize(average_score = mean(Score))
